564bbe51acd98e1c75577ddffd4bb8f41d69ef0b
Giuseppe Carteny <giuseppe.carteny@unimi.it>
2021-08-06 20:17:40 +0200

    Muted a few calls in the cz stacking script

--- Scripts/country_spec_scripts/EES2019_cz_stack.R
+++ Scripts/country_spec_scripts/EES2019_cz_stack.R
@@ -39,7 +39,7 @@ ptv_crit <-
   EES2019_cdbk_cz %>% 
   dplyr::select(partyname, Q10_PTV) 
 
-ptv_crit #8 Parties
+# ptv_crit #8 Parties
 # Parties = KDU/CSL, CSSD, ODS, KSCM, ANO, Piráti, SPD, TOP 09
 
 
@@ -49,7 +49,7 @@ votes_crit <-
   EP2019_cz %>% 
   filter(party_name!='Other parties') 
 
-votes_crit 
+# votes_crit 
 # 12 Parties
 # parties = ODS, CSSD, KSCM, KDU-CSL, TOP 09 + STAN, ANO 2011, Piráti, SPD, HLAS,
 # EU TROLL, Vedci pro CR, Coal ROZUMNÍ ND	
@@ -65,7 +65,7 @@ party <-
   na.omit() %>% 
   .$Q7
 
-party
+# party
 
 # Create the Czech Rep. EES 2019 SDM # ====================================================================
 
@@ -77,14 +77,14 @@ EES2019_cz_stack <-
 
 # Final plausibility check # ====================================================================
 
-unique(EES2019_cz_stack$countrycode)
+# unique(EES2019_cz_stack$countrycode)
 # correct country code
 
-unique(EES2019_cz$respid) %>% length()
-unique(EES2019_cz_stack$respid) %>% length()
+# unique(EES2019_cz$respid) %>% length()
+# unique(EES2019_cz_stack$respid) %>% length()
 # number of unique respondents IDs in both original data and SDM match
 
-dim(EES2019_cz_stack)
+# dim(EES2019_cz_stack)
 # 8000 by 4 data frame as expected (8 parties x 1000 respondents)
 
 # Clean the environment # ==============================================================================
