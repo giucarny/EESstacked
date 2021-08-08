19d3f73f0143a73a26a894e6610ddfa183797542
Willie Häußling <willie.haeussling@hoitmail.de>
2021-08-08 13:25:21 +0200

    SDM for Germany and Luxembourg (2021-08-08)

---
+++ Scripts/country_spec_scripts/EES2019_de_stack.R
@@ -0,0 +1,67 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Title: Script for Stacking Observations (EES 2019 Voter Study, Germany Sample) 
+# Author: W.Haeussling
+# last update: 2021-08-08
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Keep the EES 2019 Germany sample # ===================================================================
+
+EES2019_de <- 
+  EES2019 %>% 
+  filter(countrycode==1276)
+
+
+# Filter the codebook and EP elections data # ==========================================================
+
+EP2019_de <- 
+  EP2019 %>% 
+  filter(countryshort=='DE')
+
+
+EES2019_cdbk_de <- 
+  EES2019_cdbk %>% 
+  filter(countryshort=='DE')
+
+
+# Get the respondent ID codes # ========================================================================
+
+respid <- 
+  EES2019_de$respid %>% 
+  as.numeric()
+
+# Choose the relevant parties # ========================================================================
+
+
+# Check the parties about which we have the PTV variable # - - - - - - - - - - - - - - - - - - - - - - -
+
+ptv_crit <-
+  EES2019_cdbk_de %>% 
+  dplyr::select(partyname, Q10_PTV) 
+
+# Check the vote shares of parties that obtained at least one seat in the EP # - - - - - - - - - - - - -
+
+votes_crit <- 
+  EP2019_de %>% 
+  filter(party_name!='Other parties') 
+
+# Select the relevant parties # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+party <- 
+  EES2019_cdbk_de %>% 
+  dplyr::select(partyname, Q10_PTV, Q7) %>%   
+  na.omit() %>% 
+  .$Q7
+
+
+# Create the German EES 2019 SDM # ====================================================================
+
+EES2019_de_stack <- 
+  expand_grid(respid, party) %>% 
+  mutate(countrycode=EES2019_de$countrycode %>% unique,
+         stack = paste0(respid, '-', party)) %>%
+  dplyr::select(countrycode, respid, party, stack)
+
+# Clean the environment # ==============================================================================
+
+rm(list=ls(pattern='_de$|_crit$'))  
+rm(list = c('respid', 'party'))
