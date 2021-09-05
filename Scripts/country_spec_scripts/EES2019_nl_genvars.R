abedb32cc84cd61613db1312bbacdedf89083d42
Willie Häußling <willie.haeussling@hoitmail.de>
2021-09-05 23:19:25 +0200

    No change in Luxembourg. Netherlands generic dichotomous variables estimatioin script with notes due to 10,11 and 90 in Q2, Q7, Q9_rec and Q25_rec.

--- Scripts/country_spec_scripts/EES2019_nl_genvars.R
+++ Scripts/country_spec_scripts/EES2019_nl_genvars.R
@@ -1,5 +1,5 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Title: Script for Estimating Generic Variables (EES 2019 Voter Study, Luxembourgian Sample) 
+# Title: Script for Estimating Generic Variables (EES 2019 Voter Study, Malta Sample) 
 # Author: W. Haeussling
 # last update: 2021-09-05
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -7,11 +7,11 @@
 
 # Subset the EES original data frame, the SDM, and the EES codebook # ==================================
 
-cntry = 'LU'
+cntry = 'NL'
 
-EES2019_lu <- EES2019 %>% filter(countryshort==cntry)
-EES2019_stckd_lu <- EES2019_stckd %>% filter(countryshort==cntry)
-EES2019_cdbk_lu <- EES2019_cdbk %>% filter(countryshort==cntry)
+EES2019_nl <- EES2019 %>% filter(countryshort==cntry)
+EES2019_stckd_nl <- EES2019_stckd %>% filter(countryshort==cntry)
+EES2019_cdbk_nl <- EES2019_cdbk %>% filter(countryshort==cntry)
 
 rm(cntry)
 
@@ -19,14 +19,14 @@ rm(cntry)
 
 # Check first the variable of interest values
 # lapply(c('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
-#       function(vrbl) {
-#         EES2019_stckd_lu %>%
-#           dplyr::select(all_of(vrbl)) %>%
-#           mutate(across(all_of(vrbl), ~as.numeric(.))) %>%
-#           distinct})
-
-# EES2019_stckd_lu %>%
-#  dplyr::select(Q7) %>%
+#             function(vrbl) {
+#               EES2019_stckd_nl %>%
+#                 dplyr::select(all_of(vrbl)) %>%
+#                 mutate(across(all_of(vrbl), ~as.numeric(.))) %>%
+#                 distinct})
+#
+# EES2019_stckd_nl %>%
+#  dplyr::select(Q2) %>%
 #  val_labels()
 
 #Q2 also has values 10 (other parties) and 11 (none of the parties).
@@ -35,9 +35,9 @@ rm(cntry)
 #Given that these values don't refer to parties, I made no additional changes 
 #to the code. 
 
-EES2019_lu_stack <- 
-  cbind(EES2019_stckd_lu,  
-        lapply(data = EES2019_stckd_lu, 
+EES2019_nl_stack <- 
+  cbind(EES2019_stckd_nl,  
+        lapply(data = EES2019_stckd_nl, 
                X = list('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
                stack_var = 'party',
                FUN = gendic.fun) %>% 
@@ -52,15 +52,14 @@ EES2019_lu_stack <-
 #    orivar <- vrbl
 #    genvar <- paste0(vrbl, '_gen')
 #    
-#    EES2019_lu_stack %>%
+#    EES2019_nl_stack %>%
 #      dplyr::select(respid, party, all_of(orivar), all_of(genvar)) %>%
 #      print(n=100)
 #    
 #  }
-# checkdataset.fun('Q2')
+# checkdataset.fun('Q2') %>% filter(Q2== 11, Q2_gen!=0)
 
 # Clean the environment # ==============================================================================
 
-rm(list=ls(pattern='_lu$|fun'))  
-
+rm(list=ls(pattern='_nl$|fun'))  
 
