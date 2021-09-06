3e9a5acd7daeeafb89d71ae88f27381f83fcf96a
Matthias Koernig <matthiaskoernig@gmx.de>
2021-09-06 03:33:36 +0200

    Generic Dichotomous Variables Austria, Croatia, France, Ireland, Latvia, Portugal, Romania, Slovenia

---
+++ Scripts/country_spec_scripts/EES2019_pt_genvars.R
@@ -0,0 +1,64 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Title: Script for Estimating Generic Variables (EES 2019 Voter Study, Portugues Sample) 
+# Author: M.Koernig
+# last update: 2021-09-05
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+# Subset the EES original data frame, the SDM, and the EES codebook # ==================================
+
+cntry = 'PT'
+
+EES2019_pt <- EES2019 %>% filter(countryshort==cntry)
+EES2019_stckd_pt <- EES2019_stckd %>% filter(countryshort==cntry)
+EES2019_cdbk_pt <- EES2019_cdbk %>% filter(countryshort==cntry)
+
+rm(cntry)
+
+# Generic dichotomous variables estimation # ===========================================================
+
+# Check first the variable of interest values
+# lapply(c('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
+#        function(vrbl) {
+#          EES2019_stckd_pt %>%
+#            dplyr::select(all_of(vrbl)) %>%
+#            mutate(across(all_of(vrbl), ~as.numeric(.))) %>%
+#            distinct})
+# 
+# EES2019_stckd_pt %>%
+#   dplyr::select(Q2) %>%
+#   val_labels()
+
+
+EES2019_pt_stack <- 
+  cbind(EES2019_stckd_pt,  
+        lapply(data = EES2019_stckd_pt, 
+               X = list('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
+               stack_var = 'party',
+               FUN = gendic.fun) %>% 
+          do.call('cbind',.)) %>% 
+  as_tibble()
+
+# Check the dataset 
+
+# checkdataset.fun <- 
+#   function(vrbl) {
+#     
+#     orivar <- vrbl
+#     genvar <- paste0(vrbl, '_gen')
+#     
+#     EES2019_pt_stack %>%
+#       dplyr::select(respid, party, all_of(orivar), all_of(genvar)) %>%
+#       print(n=100)
+#     
+#   }
+
+# checkdataset.fun('Q25_rec')
+
+# Clean the environment # ==============================================================================
+
+rm(list=ls(pattern='_pt$|fun'))  
+
+
+
+
---
+++ Scripts/country_spec_scripts/EES2019_ro_genvars.R
@@ -0,0 +1,64 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Title: Script for Estimating Generic Variables (EES 2019 Voter Study, Romanian Sample) 
+# Author: M.Koernig
+# last update: 2021-09-05
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+# Subset the EES original data frame, the SDM, and the EES codebook # ==================================
+
+cntry = 'RO'
+
+EES2019_ro <- EES2019 %>% filter(countryshort==cntry)
+EES2019_stckd_ro <- EES2019_stckd %>% filter(countryshort==cntry)
+EES2019_cdbk_ro <- EES2019_cdbk %>% filter(countryshort==cntry)
+
+rm(cntry)
+
+# Generic dichotomous variables estimation # ===========================================================
+
+# Check first the variable of interest values
+# lapply(c('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
+#        function(vrbl) {
+#          EES2019_stckd_ro %>%
+#            dplyr::select(all_of(vrbl)) %>%
+#            mutate(across(all_of(vrbl), ~as.numeric(.))) %>%
+#            distinct})
+# 
+# EES2019_stckd_ro %>%
+#   dplyr::select(Q2) %>%
+#   val_labels()
+
+
+EES2019_ro_stack <- 
+  cbind(EES2019_stckd_ro,  
+        lapply(data = EES2019_stckd_ro, 
+               X = list('Q2', 'Q7', 'Q9_rec', 'Q25_rec'),
+               stack_var = 'party',
+               FUN = gendic.fun) %>% 
+          do.call('cbind',.)) %>% 
+  as_tibble()
+
+# Check the dataset 
+
+# checkdataset.fun <- 
+#   function(vrbl) {
+#     
+#     orivar <- vrbl
+#     genvar <- paste0(vrbl, '_gen')
+#     
+#     EES2019_ro_stack %>%
+#       dplyr::select(respid, party, all_of(orivar), all_of(genvar)) %>%
+#       print(n=100)
+#     
+#   }
+
+# checkdataset.fun('Q25_rec')
+
+# Clean the environment # ==============================================================================
+
+rm(list=ls(pattern='_ro$|fun'))  
+
+
+
+
