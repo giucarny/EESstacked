df9b095c8b5122351a7b0051b3089dc6300461ba
Giuseppe Carteny <giuseppe.carteny@gmail.com>
2021-10-25 12:02:24 +0200

    Reviewing syntvars eval workflow

--- Scripts/synteval_scripts/country_spec_scripts/EES2019_el_synteval.R
+++ Scripts/synteval_scripts/country_spec_scripts/EES2019_el_synteval.R
@@ -15,48 +15,9 @@ options(scipen = 99)
 
 rm(list = ls())
 
-# General workflow # ===================================================================================
+# Source the general workflow # ========================================================================
 
-# Load the full version of the EES 2019 voter study # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-EES2019 <- read_dta(here('Data', 'EES2019', 'ZA7581_v1-0-0.dta'))
-
-# Mutate the EES 2019 voter study # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-source(here('Scripts', 'EES2019_datamut.R'))
-
-
-# Source the auxiliary data set # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_cdbk_enh.R'))
-
-
-# Harmonize Q25 and Q9 values # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_Q25_rec.R'))
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_Q9_rec.R'))
-
-
-# Load auxiliary functions # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-source(here('Scripts', 'EES2019_stack_funs.R'))
-
-
-# Stack observations # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-invisible(
-  lapply(paste0(here('Scripts', 'country_spec_scripts'),'/', 
-                here('Scripts', 'country_spec_scripts') %>% list.files(pattern = '_stack')),
-         source)  
-)
-
-EES2019_stckd <- mget(ls(pattern = '_stack')) %>% do.call('rbind',.)
-rm(list=ls(pattern='_stack'))
-
-# Stack the original EES2019 variables # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-EES2019_stckd %<>% left_join(., EES2019, by=c('countrycode', 'respid'))
+source(here('Scripts', 'synteval_scripts', 'Synteval_gen.R'))
 
 # Country-spec workflow # ==============================================================================
 
--- Scripts/synteval_scripts/country_spec_scripts/EES2019_ie_synteval.R
+++ Scripts/synteval_scripts/country_spec_scripts/EES2019_ie_synteval.R
@@ -1,7 +1,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Title: Script for Evaluating Synthetic Variables Estimation (EES 2019 Voter Study, Irish Sample) 
 # Author: M.Körnig
-# last update: 2021-10-21
+# last update: 2021-10-25
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
@@ -16,49 +16,9 @@ options(scipen = 99)
 
 rm(list = ls())
 
-# General workflow # ===================================================================================
+# Source the general workflow # ========================================================================
 
-# Load the full version of the EES 2019 voter study # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-EES2019 <- read_dta(here('Data', 'EES2019', 'ZA7581_v1-0-0.dta'))
-
-# Mutate the EES 2019 voter study # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-source(here('Scripts', 'EES2019_datamut.R'))
-
-
-# Source the auxiliary data set # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_cdbk_enh.R'))
-
-
-# Harmonize Q25 and Q9 values # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_Q25_rec.R'))
-
-source(here('Scripts', 'aux_data_scripts', 'EES2019_Q9_rec.R'))
-
-
-# Load auxiliary functions # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-source(here('Scripts', 'EES2019_stack_funs.R'))
-
-
-# Stack observations # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-invisible(
-  lapply(paste0(here('Scripts', 'country_spec_scripts'),'/', 
-                here('Scripts', 'country_spec_scripts') %>% list.files(pattern = '_stack')),
-         source)  
-)
-
-EES2019_stckd <- mget(ls(pattern = '_stack')) %>% do.call('rbind',.)
-rm(list=ls(pattern='_stack'))
-
-
-# Stack the original EES2019 variables # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-EES2019_stckd %<>% left_join(., EES2019, by=c('countrycode', 'respid'))
+source(here('Scripts', 'synteval_scripts', 'Synteval_gen.R'))
 
 
 # Country-spec workflow # ==============================================================================
