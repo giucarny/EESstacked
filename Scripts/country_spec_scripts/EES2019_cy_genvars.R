b236ec92c06461c32f16a634d1fb2d7d417843d1
Giuseppe Carteny <giuseppe.carteny@gmail.com>
2021-10-26 13:39:17 +0200

    Still working on the tutorial and related files

--- Scripts/country_spec_scripts/EES2019_cy_genvars.R
+++ Scripts/country_spec_scripts/EES2019_cy_genvars.R
@@ -1,7 +1,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Title: Script for Estimating Generic Variables (EES 2019 Voter Study, Cypriot Sample) 
 # Author: G.Carteny
-# last update: 2021-10-03
+# last update: 2021-10-26
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
@@ -99,7 +99,7 @@ EES2019_cy_stack %<>%
 EES2019_cy_stack %<>%
   left_join(.,
             lapply(data = EES2019_cy_stack,
-                   cat.indvar =  c('D3_rec', 'D8_rec',  'D5_rec', 'EDU_rec', 'D1_rec', 'D7_rec'), 
+                   cat.indvar =  c('D3_rec', 'D8_rec',  'D5_rec', 'EDU_rec', 'D1_rec', 'D7_rec', 'D6_une'), 
                    cont.indvar =  c('D4_age', 'D10_rec'),
                    yhat.name = 'socdem_synt',
                    regsum = F,
